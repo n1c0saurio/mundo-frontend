e4c08bcfe7ae2cf8b7707c2ca6209c19a1c7ef82
Nicolás <wantobelieve@gmail.com>
2023-10-23 13:28:23 -0300

    Add basic listing of existing device fetched from the API

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { Suspense } from "react";
+import { fetchData } from "./fetchData";
+import "./App.css";
+
+const apiDispositivos = fetchData("http://localhost:8080/api/dispositivos");
 
 function App() {
+  const data = apiDispositivos.read();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Listado de dispositivos</h1>
+      <Suspense fallback={<div>Cargando...</div>}>
+        <ul className="card">
+          {data?.map((dispositivo) => (
+            <li>{dispositivo.nombre}</li>
+          ))}
+        </ul>
+      </Suspense>
     </div>
   );
 }
