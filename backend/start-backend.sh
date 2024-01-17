a3d0460aa70628a4f6bf4b14308d1b5ab8d7bda5
Nicolás <wantobelieve@gmail.com>
2024-01-16 23:50:36 -0300

    refactor: project restructuring

--- backend/start-backend.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Backend service starting..."
-
-# start php and run migrations/seeders
-php-fpm -D && php artisan migrate && php artisan db:seed
-
-# start server
-nginx -g 'daemon off;'
